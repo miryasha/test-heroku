dd124819cc2d8617338be7def71885e2fcce5e23
cat-coder <cat-coder>
2021-06-07 12:46:49 -0500

    first commit

---
+++ src/main/java/com/danco/helloheroku/controller/HelloHerokuController.java
@@ -0,0 +1,23 @@
+package com.danco.helloheroku.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloHerokuController {
+
+
+
+        @RequestMapping(value = "/hello", method = RequestMethod.GET)
+        public String doHello() {
+            return "Hello, Heroku";
+        }
+        @RequestMapping(value = "/hi/{name}", method = RequestMethod.GET)
+        public String doHi(@PathVariable String name) {
+            return "What's happening, " + name;
+        }
+
+}
+
